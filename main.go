507580a56fbc2186cb5388aa83b1671bd53c40b2
viveksahu26 <vivekkumarsahu650@gmail.com>
2022-09-14 12:28:10 +0530

    saving URL to file
    Signed-off-by: viveksahu26 <vivekkumarsahu650@gmail.com>

--- main.go
+++ main.go
@@ -3,7 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/viveksahu26/url_shortner/src"
 )
+
+func handleShortUrl(writer http.ResponseWriter, req *http.Request) {
+	// get original URL from GET method by quering
+	originalURL := req.URL.Query().Get("longURL")
+	fmt.Println("originalURL: ", originalURL)
+
+	// generate random shortURL
+	shortURL := src.GenerateShortURL()
+	fmt.Println("shortURL: ", shortURL)
+
+	// save short and long URL to file
+	src.SaveToFile(shortURL, originalURL)
+}
 
 func main() {
 	fmt.Println("URL Shorten Service")
---
+++ src/savetofile.go
@@ -0,0 +1,47 @@
+package src
+
+import (
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+func SaveToFile(shortURL string, longURL string) {
+	fileName := "url.properties"
+	prop := shortURL + "=" + longURL
+	if _, err := os.Stat(fileName); errors.Is(err, os.ErrNotExist) {
+		os.Create(fileName)
+		os.WriteFile(fileName, []byte(prop+"\n"), 0o644)
+	} else {
+
+		input, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		lines := strings.Split(string(input), "\n")
+
+		var flag bool
+		for i, line := range lines {
+			if strings.Contains(line, shortURL) {
+				lines[i] = prop
+				flag = true
+			}
+		}
+		if flag {
+			output := strings.Join(lines, "\n")
+			err = ioutil.WriteFile(fileName, []byte(output), 0o644)
+			if err != nil {
+				log.Fatalln(err)
+			}
+		} else {
+			file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+			if err != nil {
+				panic(err)
+			}
+			_, _ = file.WriteString(prop + "\n")
+		}
+	}
+}
